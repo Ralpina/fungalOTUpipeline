220fc6e0c0097b27d10d41a79db1096829f8f5e0
Roberta Gargiulo <48416466+Ralpina@users.noreply.github.com>
2023-05-31 15:57:03 +0100

    Create 1_create_dirs.sh

---
+++ Scripts/1_create_dirs.sh
@@ -0,0 +1,18 @@
+# We create directories to organise the data at the different stages of the pipeline:
+
+mkdir myco
+mkdir myco/seqs  
+mkdir myco/phred_out 
+mkdir myco/assembled
+mkdir myco/assembled_fastq
+mkdir myco/chimeras
+mkdir myco/results
+mkdir myco/database
+mkdir myco/sing
+mkdir myco/singR
+mkdir myco/sing_fastq
+mkdir myco/scripts
+mkdir myco/errors
+
+cd myco
+
