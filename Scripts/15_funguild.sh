4ccf43fb6a97fd423679e7086b7d7a3b4ff7acca
Roberta Gargiulo <48416466+Ralpina@users.noreply.github.com>
2023-08-22 14:46:19 +0100

    Create 15_funguild.sh

---
+++ Scripts/15_funguild.sh
@@ -0,0 +1,85 @@
+# Assuming we have FUNGuild.py in the directory FUNGuild, we need the following files:
+
+# results of the search of sequences against UNITE with 0.97 similarity threshold:
+cp ./results/SH_table.uc ./FUNGuild/
+
+# results of the search of de novo sequences against UNITE with 0.5 similarity threshold:
+cp results/denovo_SH_table.uc ./FUNGuild/
+
+
+cd FUNGuild/
+grep -v "^N" SH_table.uc | cut -f 9-10 > SH.uc
+grep -v "^N" denovo_SH_table.uc | cut -f 9-10 > denovo.uc 
+
+# we add the header (make sure separators are the same as for the following lines, usually tabs)
+# most importantly, we need to add "taxonomy" as the header of the taxonomic information (case-sensitive!), in the last column
+sed -i $'1 i x\ttaxonomy' SH.uc
+sed -i $'1 i x\ttaxonomy' denovo.uc
+
+
+module load python/3.7.14
+module load anaconda/2020.11 ## used instead of pandas on KewHPC
+# preparing the taxonomic table
+
+python FUNGuild.py taxa -otu SH.uc -format tsv -classifier unite
+python FUNGuild.py taxa -otu denovo.uc -format tsv -classifier unite
+# these will produce the two files SH.taxa.uc and denovo.taxa.uc
+# assigning guilds
+python FUNGuild.py guild -taxa SH.taxa.uc                                      
+python FUNGuild.py guild -taxa denovo.taxa.uc 
+# these will produce the two files: SH.taxa.guilds.txt and denovo.taxa.guilds.txt
+
+
+# now let's prepare tables that also include SH codes
+cut -f 2 SH.uc | cut -d "|" -f 3 > SH.txt
+cut -f 2 denovo.uc | cut -d "|" -f 3 > SH_denovo.txt
+paste SH.taxa.guilds.txt SH.txt > guilds.SH.txt
+paste denovo.taxa.guilds.txt SH_denovo.txt > denovo.guilds.SH.txt
+
+# let's also prepare a concatenated file to allow counting unique SHs:
+cat guilds.SH.txt denovo.guilds.SH.txt > allguilds.txt
+
+
+# we want to extract all the accessions which are assigned with some probability to a given taxon
+# we do it for the three different files, to be able to tell apart de novo from those found in UNITE
+grep "Probable" allguilds.txt | grep "Ectomycorrhizal" > allEctoguilds.txt
+grep "Probable" guilds.SH.txt | grep "Ectomycorrhizal" > Ectoguilds.txt
+grep "Probable" denovo.guilds.SH.txt | grep "Ectomycorrhizal" > Ectodenovoguilds.txt
+
+
+# to count how many SHs:
+echo "Total number of Ectomycorrhizal sequences (de novo and not)"
+wc -l allEctoguilds.txt 
+echo "Total number of de novo Ectomycorrhizal sequences (centroids)"
+wc -l Ectodenovoguilds.txt 
+# the above should actually be considered separately if we assume that two de novo centroids with the same SH could be different taxa, although with some similarity
+# in fact, remember that de novo were searched against UNITE with a low similarity threshold
+echo "Number of different SHs in all the de novo Ectomycorrhizal sequences (centroids)"
+cut -f 18 Ectodenovoguilds.txt | sort | uniq | wc -l
+echo "Number of different SHs in all Ectomycorrhizal sequences with 97% similarity to UNITE SHs"
+cut -f 18 Ectoguilds.txt | sort | uniq | wc -l
+echo "Number of different SHs in all Ectomycorrhizal sequences (de novo and not)"
+cut -f 18 allEctoguilds.txt | sort | uniq | wc -l
+# the above can be used as a rough indication but it shouldn't be used to count de novo sequences, as we assume that each de novo is self-standing because of the similarity threshold used
+
+# to extract the non-ecto sequences, we do the opposite as we did above:
+grep "Probable" allguilds.txt | grep -v "Ectomycorrhizal" > non_ecto_allguilds.txt
+grep "Probable" guilds.SH.txt | grep -v "Ectomycorrhizal" > non_ectoguilds.txt
+grep "Probable" denovo.guilds.SH.txt | grep -v "Ectomycorrhizal" > non_ectodenovoguilds.txt
+
+echo "Number of different SHs in all the de novo non-ectomycorrhizal sequences (centroids)"
+cut -f 18 non_ectodenovoguilds.txt | sort | uniq | wc -l
+echo "Number of different SHs in all non-ectomycorrhizal sequences with 97% similarity to UNITE SHs"
+cut -f 18 non_ectoguilds.txt | sort | uniq | wc -l
+echo "Number of different SHs in all non-ectomycorrhizal sequences (de novo and not)"
+cut -f 18 non_ecto_allguilds.txt | sort | uniq | wc -l
+
+# the above can be used as a rough indication but it shouldn't be used to count de novo sequences, as we assume that each de novo is self-standing because of the similarity threshold used
+
+#all other sequences, mostly unidentified
+echo "Total number of other sequences, with FUNGuild assignments possible or not available"
+grep -v "^OTU" allguilds.txt | grep -v "Probable" | wc -l
+#unique SHs in these
+echo "Total number of different SHs with FUNGuild assignments possible or not available"
+grep -v "^OTU" allguilds.txt | grep -v "Probable" | cut -f 18 | sort | uniq | wc -l
+
