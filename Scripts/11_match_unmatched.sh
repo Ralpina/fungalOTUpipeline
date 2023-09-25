d965073c84c4e115139029ad22573ff38fc09c7a
Roberta Gargiulo <48416466+Ralpina@users.noreply.github.com>
2023-09-25 10:07:36 +0100

    Update 11_match_unmatched.sh

--- Scripts/11_match_unmatched.sh
+++ Scripts/11_match_unmatched.sh
@@ -1,9 +1,5 @@
-# this script will retrieve all the sequences that were not found in UNITEv9 using a similarity threshold  = 97%,
+# this script will retrieve all the sequences that were not found in UNITEv9 using the similarity threshold  >97%,
 # and try to look for similar ones among the centroids previously obtained
-
-# let's first concatenate the singlets and contigs fasta file, to avoid getting too many output files:
-cat results/notmatched_contigs.fasta results/notmatched_singlets.fasta > ./results/notmatched.fasta
-
 
 module load vsearch/2.22.1
 vsearch --usearch_global results/notmatched.fasta \
